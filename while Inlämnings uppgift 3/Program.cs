9ba319b58648a03d0e9ff028b8eb813a7b937265
molo06002 <molo06002@E-R90YE07N>
2023-10-20 10:15:28 +0200

    Add project files.

---
+++ while Inlämnings uppgift 3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace while_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            while (max < 100)
+            {
+                max++;
+                Console.WriteLine(max);
+                if (max % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (max % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (max % 5 == 0 &&  max % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+            }
+        }
+    }
+}
